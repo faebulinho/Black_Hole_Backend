fe0a7cfabfb903570eb0860b7add99da52814569
faebulinho <fabian.boner@outlook.com>
2025-03-28 23:01:51 +0100

    ports matched

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ dotenv.config();
 export const prisma = new PrismaClient();
 
 export const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.json());
